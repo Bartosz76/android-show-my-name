c49017b31e3f50dfb3a5fbc3925e8841cdebf7cf
Bartosz76 <mierzwinski.b@gmail.com>
2021-08-18 12:19:08 +0200

    The Show Name button is now interactive.

--- app/src/main/java/com/example/show_my_name/MainActivity.java
+++ app/src/main/java/com/example/show_my_name/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.show_my_name;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Instance variables to be joined with widgets via their IDs.
+     */
     private Button showButton;
     private TextView nameText;
 
@@ -16,9 +20,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /**
+         * Performing the linkage!
+         */
         showButton = findViewById(R.id.button);
         nameText = findViewById(R.id.textView);
 
-        nameText.setText("Hello from Java!");
+        /**
+         * Setting an "event listener" on my button.
+         */
+        showButton.setOnClickListener(new View.OnClickListener() {
+            /**
+             * When the user taps on the button...
+             */
+            @Override
+            public void onClick(View view) {
+                /**
+                 * ... this happens!
+                 */
+                nameText.setText("Hello, Bartosz!");
+            }
+        });
     }
 }
