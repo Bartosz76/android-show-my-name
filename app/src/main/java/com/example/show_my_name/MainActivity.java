2350f566564419c063a89d8649ed82efb7afb000
Bartosz76 <mierzwinski.b@gmail.com>
2021-08-18 11:56:53 +0200

    Added and modified first buttons.

--- app/src/main/java/com/example/show_my_name/MainActivity.java
+++ app/src/main/java/com/example/show_my_name/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.show_my_name;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button showButton;
+    private TextView nameText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showButton = findViewById(R.id.button);
+        nameText = findViewById(R.id.textView);
+
+        nameText.setText("Hello from Java!");
     }
 }
